fab00cb8d6368812f54d55dd77f12654631b4553
h. De Kesel <hannes.dekesel@tsaam.be>
2025-12-01 21:56:03 +0100

    feedback

--- controler/spotten.js
+++ controler/spotten.js
@@ -1,44 +1,51 @@
-const vogels = require("../data/index")
-const getVogels = (req,res) => {
-    let AllVogels = []
-    vogels.forEach((vogel) => {
-        AllVogels.push({id:vogel.id,soort:vogel.soort})
-        
-    })
-    res.send(AllVogels)
-}
+const vogels = require("../data/index");
+const getVogels = (req, res) => {
+  // Je werkt graag met foreach - maar met een map is het eenvoudiger
+  let AllVogels = [];
+  vogels.forEach((vogel) => {
+    AllVogels.push({ id: vogel.id, soort: vogel.soort });
+  });
+  res.send(AllVogels);
+};
 
-const getOneVogel = (req,res) => {
-    const getID = req.params.ID
-    const gevondenVogel = vogels.find((vogel) => vogel.id == getID)
-    res.json(gevondenVogel)
-}
+const getOneVogel = (req, res) => {
+  const getID = req.params.ID;
+  const gevondenVogel = vogels.find((vogel) => vogel.id == getID);
+  res.json(gevondenVogel);
+};
 
-const vogelGespot = (req,res) => {
-    let aantalKeerGespot = 0
-    vogels.forEach((vogel) => {
-        if (vogel.aantalKeerGespot > aantalKeerGespot){
-            aantalKeerGespot = vogel.aantalKeerGespot
-        }
-    })
-    const gevondenVogel = vogels.find((vogel) => vogel.aantalKeerGespot == aantalKeerGespot)
-    console.log(aantalKeerGespot);
-    
-    res.json(gevondenVogel)
-}
-const vogelGespotPlus1 = (req,res) => {
-    const getID = req.body.id
-    const index = vogels.findIndex((vogel) => vogel.id == getID)
-    const gevondenVogel = vogels.find((vogel) => vogel.id == getID)
-    gevondenVogelPlus1 = gevondenVogel.aantalKeerGespot++
-    gevondenVogel.aantalKeerGespot[index] = gevondenVogelPlus1
-    res.json(gevondenVogel)
+// wat doe je hier? Wat is het doel van deze functie?
+const vogelGespot = (req, res) => {
+  let aantalKeerGespot = 0;
+  vogels.forEach((vogel) => {
+    if (vogel.aantalKeerGespot > aantalKeerGespot) {
+      aantalKeerGespot = vogel.aantalKeerGespot;
+    }
+  });
+  const gevondenVogel = vogels.find(
+    (vogel) => vogel.aantalKeerGespot == aantalKeerGespot
+  );
+  console.log(aantalKeerGespot);
 
-}
+  res.json(gevondenVogel);
+};
+
+const vogelGespotPlus1 = (req, res) => {
+  const getID = req.body.id;
+
+  // waarom zoek je eerst de index ...
+  const index = vogels.findIndex((vogel) => vogel.id == getID);
+  const gevondenVogel = vogels.find((vogel) => vogel.id == getID);
+  gevondenVogelPlus1 = gevondenVogel.aantalKeerGespot++;
+  // Je werkt met een array, een array is reference type dus moet je
+  //   niet nog via de index werken.
+  gevondenVogel.aantalKeerGespot[index] = gevondenVogelPlus1;
+  res.json(gevondenVogel);
+};
 
 module.exports = {
-    getVogels,
-    getOneVogel,
-    vogelGespot,
-    vogelGespotPlus1
-}
+  getVogels,
+  getOneVogel,
+  vogelGespot,
+  vogelGespotPlus1,
+};
--- routes/spotten.js
+++ routes/spotten.js
@@ -1,13 +1,15 @@
 const express = require("express");
 // maak router object aan
 const routes = express.Router();
-const ctrlspotten = require("../controler/spotten")
+const ctrlspotten = require("../controler/spotten");
 
 // maak de verschillende routs aan
 
 routes.get("/", ctrlspotten.getVogels);
 routes.get("/:ID", ctrlspotten.getOneVogel);
-routes.get("/vogel",ctrlspotten.vogelGespot)
+// dit is een foutje die jullie moesten ontdekken.
+// Als je reeds een route get rout hebt /:ID kan je niet nog s /vogel hebben.
+routes.get("/vogel", ctrlspotten.vogelGespot);
 routes.put("/", ctrlspotten.vogelGespotPlus1); // {ID}
 
 // exporteer het router object
